ed3e2ef2a2e957d587139f5c10327ec01d266478
Nitin Prakash <prakash.nitin63@gmail.com>
2024-06-30 09:03:31 +0530

    Remove emacs + add gitconfig

--- emacs/lisp/init-local.el
+++
@@ -1,8 +0,0 @@
-;;; init-local.el --- Local config -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(remove-hook 'sanityinc/lispy-modes-hook 'enable-paredit-mode)
-
-(provide 'init-local)
-;;; init-local.el ends here
